a6a89ef07e6f17b97496167e7cd7a66812baf560
LeCheWang <haphonggoroki@gmail.com>
2024-01-05 23:30:56 +0700

    add cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const router = require('./routers');
 
+app.use(cors);
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
