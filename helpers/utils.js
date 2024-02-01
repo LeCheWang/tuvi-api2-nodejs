239a939c986de4e77a6bfa5e2afdcbb34621113f
LeCheWang <haphonggoroki@gmail.com>
2024-02-01 15:44:56 +0700

    fix tuoi

--- helpers/utils.js
+++ helpers/utils.js
@@ -2,22 +2,25 @@ function getAge(ngay, thang, nam) {
   // Lấy ngày hiện tại
   var ngayHienTai = new Date();
 
+  const soTuoi = ngayHienTai.getFullYear() - nam + 1;
+
   // Tạo đối tượng Date từ thông tin ngày sinh
-  var ngaySinh = new Date(nam, thang - 1, ngay);
+  // var ngaySinh = new Date(nam, thang - 1, ngay);
 
-  // Tính toán số tuổi
-  var soTuoi = ngayHienTai.getFullYear() - ngaySinh.getFullYear();
+  // // Tính toán số tuổi
+  // var soTuoi = ngayHienTai.getFullYear() - ngaySinh.getFullYear();
 
-  // Kiểm tra xem đã qua sinh nhật chưa trong năm nay
-  if (
-    ngayHienTai.getMonth() < ngaySinh.getMonth() ||
-    (ngayHienTai.getMonth() === ngaySinh.getMonth() &&
-      ngayHienTai.getDate() < ngaySinh.getDate())
-  ) {
-    soTuoi--;
-  }
+  // // Kiểm tra xem đã qua sinh nhật chưa trong năm nay
+  // if (
+  //   ngayHienTai.getMonth() < ngaySinh.getMonth() ||
+  //   (ngayHienTai.getMonth() === ngaySinh.getMonth() &&
+  //     ngayHienTai.getDate() < ngaySinh.getDate())
+  // ) {
+  //   soTuoi--;
+  // }
 
   return soTuoi;
 }
 
 module.exports = { getAge };
+ 
